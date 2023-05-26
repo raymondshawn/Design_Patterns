29e03fdbaebeb7445bfa7d41b814f187cad77c7b
raymond <raymondshawn779@gmail.com>
2023-05-26 05:24:14 +0300

    after  implementing factory design pattern

--- Factory Design Pattern/factory.py
+++ Factory Design Pattern/factory.py
@@ -22,12 +22,16 @@ class Truck:
         print("Hauling with the truck.")
 
 
-def create_vehicle(vehicle_type):
-    if vehicle_type == "car":
-        return Car()
-    elif vehicle_type == "motorcycle":
-        return Motorcycle()
-    elif vehicle_type == "truck":
-        return Truck()
-    else:
-        raise ValueError("Invalid vehicle type.")
+class VehicleFactory:
+
+    @staticmethod
+    def create_vehicle(vehicle_type):
+
+        if vehicle_type == "car":
+            return Car()
+        elif vehicle_type == "motorcycle":
+            return Motorcycle()
+        elif vehicle_type == "truck":
+            return Truck()
+        else:
+            raise ValueError("Invalid vehicle type.")
